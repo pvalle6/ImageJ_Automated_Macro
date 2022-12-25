f5389478fb2f54f547d826a636b251358160e7e6
Peter Vallet <pvalle6@lsu.edu>
2022-12-25 11:18:28 +0000

    Script Working

--- code/Lane_Request_TIF2JPEG/Search_Convert_CopyImage_V1.py
+++ code/Lane_Request_TIF2JPEG/Search_Convert_CopyImage_V1.py
@@ -1,86 +1,41 @@
-import os
-import shutil
-import pyimagej
-
-# Peter Vallet, Jung Lab, 2022 
-# Search_Convert_CopyImage V1
+# Peter Vallet, Jung Lab, 2022
+# Search_Convert_CopyImage 
 # Script to convert TIF2JPEGs
 # 1. Copies Parent Folder to renamed Parent Folder
 # 2. Recursively Searches and runs Image Conversion Macro on TIF
 # 3. Prints message Confirming Success
 # Final is UNIX/MACOS compatible
 
-ij = imagej.init('sc.fiji:fiji')
+import imagej #imports necessary imagej module
+import os #used in date, directory naming
+from datetime import date
+day = date.today()
 
-## 		set this equal to parent folder of where you want to convert
-OG_parent_path = R"C:\Users\valle\OneDrive\Documents\EP-DE\data\laneTestInput\tif_files" #<<<<<<this one
-######################
-## 		don't touch below
-new_parent_path = R"C:\Users\valle\OneDrive\Documents\EP-DE\data\laneTestInput\jpep_files"
-##		creates a list from the subdirectories in given parent
-##		creates an empty list to iterate new path files into 
-OGsubdirectories = os.listdir(OG_parent_path)
-newsubdirectories = []
-## 		checks for the parent folder with images and names a copied
-##					folder if found
-##		does this by looping through original using x, y for copy creation
-for x, y in enumerate(subdirectories):
-	if not (os.path.isdir(OGsubdirectories[x])):
-		raise ValueError("Original Path Name does not Exist. ~Search_Convert_Copy")
-	else:
-		#pretty sure that new_parent_path needs to be calling a list of changning subparents
-		newsubdirectories[y] = (x+ R'\_JPEG_converted')
+ij = imagej.init('sc.fiji:fiji') #activates imagej
 
-## 		checks if a new folder exists, if not, makes it based on
-## 					original folders name 
+macro = """
+#@ String input
+#@ String output
+#@output Object greeting
+open(input);
+        saveAs("Jpeg", output);
+        close();
+"""
+def search_copy(subLoc):
+    New_Directory = os.path.join(os.path.dirname(subLoc),('JPEG_Formatted_ImageJ_' + str(day)))
+    for dirpath, dirnames, filenames in os.walk(subLoc):
+        New_Loc = New_Directory + '/' + os.path.basename(dirpath)
+        os.makedirs(New_Loc)
+        #print(New_Loc)
+        for files in filenames:  
+            args ={
+            'input': os.path.join(dirpath,files),
+            'output': os.path.join(New_Loc, files)}
+            ij.py.run_macro(macro, args)
+directoryLoc = '/workspaces/ImageJ_Automated_Macro/code/test_dir/omega' # directory to be copied
+search_copy(directoryLoc)
 
-##		possible not needed if already implemented into macro 
-##					Image_Processing_Pablo_for_Phillip.ijm
-## 					runs using ijm
-if not (os.path.isdir(new_parent_path)):
-	os.makedirs(new_parent_path)
-## 		copies over file into renamed alternative folder
-#copy_tree(OG_parent_path, new_parent_path)
-
-# IMPLEMENT OS.WALK TO SEARCH PARENT DIRECTORY 
-# AND CREATE A TUPLE WITH SUBDIRECTORIES
-
-
-##		Code below is modified from a script by Pablo Hofbauer for Phillip Jung
-## 		Code takes a ImageJ macro as a string, then by use of arguments 
-##					in a dictionary, calls new folder name the macro 
-##					is then run on that folder, converting to JPEG
-##					and then saving to a new folder
-
-# macro = """ 
-# #@input 
-# #@output
-# #@folderCount
-# function action(input, output, filename) 
-# {
-#         open(input + filename);
-#         saveAs("Jpeg", output + filename);
-#         close();
-# }
-# // runs the script to convert
-# setBatchMode(true); 
-# 	list = getFileList(input);
-# 	for (i = 0; i < list.length; i++)
-# action(input, output, list[i]);
-# 	setBatchMode(false);
-
-# exit("Files succesfully saved!")
-# """
-# count = 0
-# args = {
-# 	'input': OGsubdirectories[count],
-# 	'output': newsubdirectories[count],
-#  	'folderCount': count++ 
-# }
-
-
-
-# # NEED TO IMPLEMENT FUNCTION CALL FOR EVERY 
-# # FOLDER IN DIRECTORY THAT NEEDS TO BE CONVERTED
-
-# ij.py.run_macro(macro, args)
+""" for names in dirnames:
+            New_Loc = os.path.join(New_Directory,names)
+            if not os.path.exists(New_Loc):
+                os.makedirs(New_Loc) """
--- code/Lane_Request_TIF2JPEG/test_import.py
+++ code/Lane_Request_TIF2JPEG/test_import.py
@@ -1,1 +1,2 @@
 import imagej
+ij = imagej.init("2.5.0"); print(ij.getVersion())
