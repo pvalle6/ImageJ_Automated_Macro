1ba6854132b2ec0cbf4f70b1821d94d561a4ff95
Peter Vallet <pvalle6@lsu.edu>
2022-12-25 21:14:21 -0600

    Update search_convert.py

--- code/Lane_Request_TIF2JPEG/search_convert.py
+++ code/Lane_Request_TIF2JPEG/search_convert.py
@@ -2,29 +2,27 @@ import os #used in date, directory naming
 from datetime import date
 import imagej #imports necessary imagej module
 day = date.today()
-
 ij = imagej.init('sc.fiji:fiji') #activates imagej
-
 # MACRO USED IN PyImageJ
 MACRO = """
 #@ String input
 #@ String output
 #@output Object greeting
 open(input);
-        saveAs("Jpeg", output);
-        close();
+saveAs("Jpeg", output);
+close();
 """
 day = date.today()
 def search_copy(sub_loc): 
-        """ Searches and Copies a Directory while running each file through PyImageJ """
-        new_directory = os.path.join(os.path.dirname(sub_loc),('JPEG_Formatted_ImageJ_' + str(day)))
+        """Searches and Copies a Directory while running each file through PyImageJ"""
+        new_directory = os.path.join(os.path.dirname(sub_loc),('JPEG_Formatted_ImageJ_'+str(day)))
         for dirpath, dirnames, filenames in os.walk(sub_loc):
-                new_loc = new_directory + '/' + os.path.basename(dirpath)
+                new_loc = new_directory+'/'+os.path.basename(dirpath)
                 os.makedirs(new_loc)
                 for files in filenames:  
-                    args ={
+                    args={
                     'input': os.path.join(dirpath,files),
                     'output': os.path.join(new_loc, files)}
                     ij.py.run_macro(MACRO, args)
-DIRECTORY_LOC = '/workspaces/ImageJ_Automated_Macro/code/test_dir/omega' # directory to be copied
+DIRECTORY_LOC='/workspaces/ImageJ_Automated_Macro/code/test_dir/omega'
 search_copy(DIRECTORY_LOC)
