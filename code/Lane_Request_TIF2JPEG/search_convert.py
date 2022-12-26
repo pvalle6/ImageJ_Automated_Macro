aca9a4d36709ddeb6c86bf3f6d93ca0e5d726bb1
Peter Vallet <pvalle6@lsu.edu>
2022-12-25 21:08:18 -0600

    Update search_convert.py

--- code/Lane_Request_TIF2JPEG/search_convert.py
+++ code/Lane_Request_TIF2JPEG/search_convert.py
@@ -17,14 +17,14 @@ open(input);
 day = date.today()
 def search_copy(sub_loc): 
         """ Searches and Copies a Directory while running each file through PyImageJ """
-    new_directory = os.path.join(os.path.dirname(sub_loc),('JPEG_Formatted_ImageJ_' + str(day)))
-    for dirpath, dirnames, filenames in os.walk(sub_loc):
-        new_loc = new_directory + '/' + os.path.basename(dirpath)
-        os.makedirs(new_loc)
-        for files in filenames:  
-            args ={
-            'input': os.path.join(dirpath,files),
-            'output': os.path.join(new_loc, files)}
-            ij.py.run_macro(MACRO, args)
+        new_directory = os.path.join(os.path.dirname(sub_loc),('JPEG_Formatted_ImageJ_' + str(day)))
+        for dirpath, dirnames, filenames in os.walk(sub_loc):
+                new_loc = new_directory + '/' + os.path.basename(dirpath)
+                os.makedirs(new_loc)
+                for files in filenames:  
+                    args ={
+                    'input': os.path.join(dirpath,files),
+                    'output': os.path.join(new_loc, files)}
+                    ij.py.run_macro(MACRO, args)
 DIRECTORY_LOC = '/workspaces/ImageJ_Automated_Macro/code/test_dir/omega' # directory to be copied
 search_copy(DIRECTORY_LOC)
