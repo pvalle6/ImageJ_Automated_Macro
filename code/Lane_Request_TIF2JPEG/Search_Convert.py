3b7acb34bef97ee5f2f09f634bbfcdcfdc72b85d
Peter <pvalle6@lsu.edu>
2022-12-25 20:56:59 -0600

    cleaned up

--- code/Lane_Request_TIF2JPEG/Search_Convert.py
+++ code/Lane_Request_TIF2JPEG/Search_Convert.py
@@ -1,14 +1,6 @@
-# Peter Vallet, Jung Lab, 2022
-# Search_Convert_CopyImage 
-# Script to convert TIF2JPEGs
-# 1. Copies Parent Folder to renamed Parent Folder
-# 2. Recursively Searches and runs Image Conversion Macro on TIF
-# 3. Prints message Confirming Success
-# Final is UNIX/MACOS compatible
-
-import imagej #imports necessary imagej module
 import os #used in date, directory naming
 from datetime import date
+import imagej #imports necessary imagej module
 day = date.today()
 
 ij = imagej.init('sc.fiji:fiji') #activates imagej
@@ -22,17 +14,16 @@ open(input);
         saveAs("Jpeg", output);
         close();
 """
-
-def search_copy(subLoc): ## function used 
-    New_Directory = os.path.join(os.path.dirname(subLoc),('JPEG_Formatted_ImageJ_' + str(day)))
-    for dirpath, dirnames, filenames in os.walk(subLoc):
+day = date.today()
+def search_copy(sub_loc): ## function used 
+    New_Directory = os.path.join(os.path.dirname(sub_loc),('JPEG_Formatted_ImageJ_' + str(day)))
+    for dirpath, dirnames, filenames in os.walk(sub_loc):
         New_Loc = New_Directory + '/' + os.path.basename(dirpath)
-        os.makedirs(New_Loc)
-        #print(New_Loc)
+        os.makedirs(new_loc)
         for files in filenames:  
             args ={
             'input': os.path.join(dirpath,files),
-            'output': os.path.join(New_Loc, files)}
+            'output': os.path.join(new_loc, files)}
             ij.py.run_macro(macro, args)
-directoryLoc = '/workspaces/ImageJ_Automated_Macro/code/test_dir/omega' # directory to be copied
-search_copy(directoryLoc)
+DIRECTORY_LOC = '/workspaces/ImageJ_Automated_Macro/code/test_dir/omega' # directory to be copied
+search_copy(DIRECTORY_LOC)
